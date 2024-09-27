3ede4dfc67f733a048535114ac49c1ec07e87bc0
Soney <65457419+sonephyo@users.noreply.github.com>
2024-09-27 00:44:59 -0400

    chore: setting up backend(Springboot and frontend(Nextjs)

---
+++ Project1_CodeSource/genetic-algorithm-project1-backend/src/main/java/com/csc375/genetic_algorithm_project1_backend/GeneticAlgorithmProject1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.csc375.genetic_algorithm_project1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneticAlgorithmProject1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneticAlgorithmProject1BackendApplication.class, args);
+	}
+
+}
