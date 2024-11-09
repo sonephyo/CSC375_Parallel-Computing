7a2bc9d8ac9c37c456fd9701ab30e0e5b6bd6053
Soney <65457419+sonephyo@users.noreply.github.com>
2024-11-09 17:41:05 -0500

    feat: setting up react with d3.js

---
+++ PerformanceMeasurement/performance-measurement-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import BarChart from './components/BarChart'
+
+
+function App() {
+
+  return (
+    <div className="App">
+    <h1>Hello React + D3 world!</h1>
+    <BarChart/>
+    </div>
+  )
+}
+
+export default App
