edcec4f728d69201120de751669ff2d2a91feeac
Soney <65457419+sonephyo@users.noreply.github.com>
2024-11-09 19:42:11 -0500

    feat: setting up cors configuratin

--- PerformanceMeasurement/performance-measurement-backend/src/main/java/com/csc375/performance_measurement_backend/config/SecurityConfig.java
+++ PerformanceMeasurement/performance-measurement-backend/src/main/java/com/csc375/performance_measurement_backend/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
