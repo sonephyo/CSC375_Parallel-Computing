824592f492904fc22899fcf96dda872ac8ce1492
Soney <65457419+sonephyo@users.noreply.github.com>
2024-11-09 19:42:46 -0500

    test: testing the http requests via postmang

--- PerformanceMeasurement/performance-measurement-backend/src/main/java/com/csc375/performance_measurement_backend/controllers/test/Tester.java
+++ PerformanceMeasurement/performance-measurement-backend/src/main/java/com/csc375/performance_measurement_backend/controllers/test/Tester.java
@@ -1,2 +1,16 @@
-package com.csc375.performance_measurement_backend.controllers.test;public class Tester {
+package com.csc375.performance_measurement_backend.controllers.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Tester {
+
+    @GetMapping("/")
+    public String sendTest() {
+        return "Hello World";
+    }
+
+
 }
