983298273c6ef521d6f0a289e87f93830fa4b7d3
Soney <65457419+sonephyo@users.noreply.github.com>
2024-11-11 10:12:46 -0500

    feat: installing axios

--- PerformanceMeasurement/performance-measurement-frontend/src/App.tsx
+++ PerformanceMeasurement/performance-measurement-frontend/src/App.tsx
@@ -1,21 +1,18 @@
-
-import './App.css'
-import BarChart from './components/BarChart'
-import BarChart2 from './components/BarChart2'
-
+import "./App.css";
+import BarChart from "./components/BarChart";
+import BarChart2 from "./components/BarChart2";
+import PerformanceMeasurement from "./components/PerformanceMeasurement";
 
 function App() {
-
   return (
     <div className="App">
-    <h1>Hello React + D3 world!</h1>
-    <BarChart/>
-    <h1>
-      Another chart
-    </h1>
-    <BarChart2/>
+      <PerformanceMeasurement/>
+      <h1>Hello React + D3 world!</h1>
+      <BarChart />
+      <h1>Another chart</h1>
+      <BarChart2 />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
