6a1f2f27a8271beffd6e61a8d57f6f4f838e3160
Soney <65457419+sonephyo@users.noreply.github.com>
2024-11-23 20:41:26 -0500

    chore: initialization of project3

---
+++ HeatPropagation/HeatPropagationSolution/src/MetalAlloy.java
@@ -0,0 +1,13 @@
+public class MetalAlloy {
+
+    private double[][] metalAlloyTemps;
+    private MetalAlloySegment[][] metalAlloySegments;
+
+    private double
+
+    public MetalAlloy(double[][] metalAlloyTemps, MetalAlloySegment[][] metalAlloySegments) {
+        this.metalAlloyTemps = metalAlloyTemps;
+        this.metalAlloySegments = metalAlloySegments;
+    }
+
+}
---
+++ HeatPropagation/HeatPropagationSolution/src/MetalAlloySegment.java
@@ -0,0 +1,12 @@
+public class MetalAlloySegment {
+
+    private double metal1Percentage;
+    private double metal2Percentage;
+    private double metal3Percentage;
+
+    public MetalAlloySegment(double metal1Percentage, double metal2Percentage, double metal3Percentage) {
+        this.metal1Percentage = metal1Percentage;
+        this.metal2Percentage = metal2Percentage;
+        this.metal3Percentage = metal3Percentage;
+    }
+}
